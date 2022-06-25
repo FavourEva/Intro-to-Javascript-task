6d474a4a11e776570870cf4ea3be80026834403e
FavourEva <favoureva118@gmail.com>
2022-06-25 11:36:23 +0100

    1st commit

---
+++ index.js
@@ -0,0 +1,10 @@
+// Write a javascript code that prints out your name, height, and country on the screen
+
+let fullName="Onyeocha Favour Nwamaka"
+alert(fullName)
+
+let height="179.83cm"
+alert(height)
+
+let country="Nigeria"
+alert(country)
